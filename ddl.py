54cda487375db3d8aa96a708297b426a66ac836f
CleoniceSoares <35998428+CleoniceSoares@users.noreply.github.com>
2020-04-09 12:33:46 -0300

    Atualização banco de dados

--- ddl.py
+++ ddl.py
@@ -9,10 +9,10 @@ cursor.execute("""
 	id_empresa INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	nome VARCHAR(45) NOT NULL,
 	id_endereco INTEGER NOT NULL,
-	id_produto INTEGER NOT NULL,
-	email VARCHAR(255) NOT NULL,
+	Email VARCHAR(255) NOT NULL,
 	telefone INTEGER NOT NULL,
-	instagram VARCHAR(255) NOT NULL
+	instagram VARCHAR(255) NOT NULL,
+	FOREIGN KEY(id_endereco) REFERENCES tb_endereco(id_endereco)
 	);
 """)
 
@@ -40,8 +40,11 @@ cursor.execute("""
 
 cursor.execute("""
 	CREATE TABLE tb_empresa_produto(
+	id_empresa_produto INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	id_empresa INTEGER NOT NULL,
-	id_produto INTEGER NOT NULL
+	id_produto INTEGER NOT NULL,
+	FOREIGN KEY(id_empresa) REFERENCES tb_empresa(id_empresa),
+	FOREIGN KEY(id_produto) REFERENCES tb_produto(id_produto)
 	);
 """)
 
